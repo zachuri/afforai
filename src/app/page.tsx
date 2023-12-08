c68634e4259b41b558ace1ea8c3fdb48ef0d624a
zachuri <punsalang.zachary@gmail.com>
2023-12-08 15:14:23 -0800

    landing page 8: completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import Landing4 from "@/components/landing-4";
 import Landing5 from "@/components/landing-5";
 import MasterHead from "@/components/mast-head";
 import Users from "@/components/users";
-import Landing7 from '@/components/landing-7';
+import Landing7 from "@/components/landing-7";
+import Landing8 from '@/components/landing-8';
 
 export default function Home() {
 	return (
@@ -16,6 +17,7 @@ export default function Home() {
 			<Landing5 />
 			{/* <Landing6 /> */}
 			<Landing7 />
+			<Landing8 />
 		</main>
 	);
 }
--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -49,4 +49,49 @@ export const Icons = {
 			/>
 		</svg>
 	),
+	fastForward: () => (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth='1.5'
+			stroke='currentColor'
+			className='w-6 h-6'>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z'
+			/>
+		</svg>
+	),
+	power: () => (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth='1.5'
+			stroke='currentColor'
+			className='w-6 h-6'>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M5.636 5.636a9 9 0 1012.728 0M12 3v9'
+			/>
+		</svg>
+	),
+	magnifyPlus: () => (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth='1.5'
+			stroke='currentColor'
+			className='w-6 h-6'>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6'
+			/>
+		</svg>
+	),
 };
